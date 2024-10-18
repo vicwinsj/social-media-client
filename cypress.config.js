9c84f3db19eedd3cabe2cf41227196a9afca947f
Victor Winsjansen <victor.winsjansen@icloud.com>
2024-10-18 18:51:41 +0700

    Cypress config update

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require('cypress');
 
+/* eslint-disable no-unused-vars */
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://127.0.0.1:5500',
     watchForFileChanges: true,
     specPattern: 'cypress/e2e/**/*.cy.js',
+    defaultCommandTimeout: 20000,
+    setupNodeEvents(on, config) {
+      
+    },
   },
 });
+/* eslint-enable no-unused-vars */
